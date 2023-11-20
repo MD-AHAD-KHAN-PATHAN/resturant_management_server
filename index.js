360f9b3e07d672234adf6da3150d5de25adf48e0
MD-AHAD-KHAN-PATHAN <md.ahadkhanpathan.tct@gmail.com>
2023-11-21 04:18:33 +0600

    create recipe add api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
@@ -10,8 +11,6 @@ const port = process.env.PORT || 5000;
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xwdt30p.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -34,7 +33,69 @@ async function run() {
     const reviewsCollection = client.db('bistroBoss').collection('reviews');
     const cartCollection = client.db('bistroBoss').collection('carts');
 
+    // JWT RELATED API
+
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res.send({token});
+    })
+    
+    // MIDDLEWARES
+    const verifyToken = (req, res, next) => {
+
+      // console.log('inside verify Token : ',req.headers.authorization);
+
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'unauthorize access'});
+      }
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+          return res.status(401).send({message: 'unauthorize access'});
+        }
+
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({message: 'forbideen access'})
+      }
+      next();
+    }
+
+
     // USER RELATED API
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+      
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      const query = {email: email}
+      const user = await userCollection.findOne(query);
+
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin'
+      }
+
+      res.send({admin})
+
+    })
+
     app.post('/users', async (req, res) => {
       const user = req.body;
 
@@ -49,12 +110,7 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/users', async (req, res) => {
-      const result = await userCollection.find().toArray();
-      res.send(result);
-    })
-
-    app.patch('/users/admin/:id', async (req, res) => {
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)};
       const updatedDoc = {
@@ -66,11 +122,19 @@ async function run() {
       res.send(result);
     })
 
-    app.delete('/users/:id', async (req, res) => {
+    app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
       const result = await userCollection.deleteOne(query);
       res.send(result);
+    })
+
+    app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
+
+      const item = req.body;
+      const result = await menuCollection.insertOne(item);
+      res.send(result);
+
     })
 
     app.get('/menu', async (req, res) => {
